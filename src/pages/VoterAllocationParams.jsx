796e6f4deccf8f5f618bf2bb710cf97864206e62
olwethumlimi <60299931+olwethumlimi@users.noreply.github.com>
2022-02-08 22:35:04 +0200

    input name updated

--- src/pages/VoterAllocationParams.jsx
+++ src/pages/VoterAllocationParams.jsx
@@ -41,7 +41,7 @@ export default function VoterAllocationParams() {
             <h5 className="m-0">VoterAllocationParams</h5>
             <span className="block mt-2 md:mt-0 p-input-icon-left">
                 <i className="pi pi-search" />
-                <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Keyword Search" />
+                <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Search By Event Name" />
             </span>
         </div>
     );
