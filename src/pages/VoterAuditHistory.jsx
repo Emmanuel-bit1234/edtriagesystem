ffb3a6314c874619e4779bdd2450085ad1b6164f
olwethumlimi <suprise.mlimi97@gmail.com>
2022-02-11 10:15:31 +0200

    updated user params

--- src/pages/VoterAuditHistory.jsx
+++ src/pages/VoterAuditHistory.jsx
@@ -1,209 +1,262 @@
-import { FilterMatchMode, FilterOperator } from "primereact/api";
-import { Button } from "primereact/button";
-import { Column } from "primereact/column";
-import { DataTable } from "primereact/datatable";
-import { Dialog } from "primereact/dialog";
-import { InputText } from "primereact/inputtext";
-import React, { useRef, useState } from "react";
-import VoterAuditHistoryServices from "../service/VoterAuditHistoryServices";
-import { TabPanel, TabView } from "primereact/tabview";
-import { Toolbar } from "primereact/toolbar";
-import { Toast } from "primereact/toast";
+import { FilterMatchMode, FilterOperator } from 'primereact/api'
+import { Button } from 'primereact/button'
+import { Column } from 'primereact/column'
+import { DataTable } from 'primereact/datatable'
+import { Dialog } from 'primereact/dialog'
+import { InputText } from 'primereact/inputtext'
+import React, { useRef, useState } from 'react'
+import VoterAuditHistoryServices from '../service/VoterAuditHistoryServices'
+import { TabPanel, TabView } from 'primereact/tabview'
+import { Toolbar } from 'primereact/toolbar'
+import { Toast } from 'primereact/toast'
 
 export const VoterAuditHistory = () => {
-    const [showDialog, setShowDialog] = useState(false);
-    const toast = useRef(null);
+  const [showDialog, setShowDialog] = useState(false)
+  const [loading, setLoading] = useState(false)
 
-    let [data, setData] = useState([]);
-    var [selectedUser, setSelectedUser] = useState([]);
+  const toast = useRef(null)
 
-    const [idNumber, setIdNumber] = useState("");
-    var voterAuditHistory = new VoterAuditHistoryServices();
+  let [data, setData] = useState([])
+  var [selectedUser, setSelectedUser] = useState([])
 
-    const filters = {
-        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        name: {
-            operator: FilterOperator.AND,
-            constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
-        },
-    };
+  const [idNumber, setIdNumber] = useState('')
+  var voterAuditHistory = new VoterAuditHistoryServices()
 
-    const submitForm = () => {
-        if (isNaN(idNumber.trim())) {
-            toast.current.show({ severity: "error", summary: "Error Message", detail: "please fill the required field", life: 3000 });
-            return false;
-        }
+  const filters = {
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    name: {
+      operator: FilterOperator.AND,
+      constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
+    },
+  }
 
-        voterAuditHistory.getAuditHistoryByID(idNumber).then((e) => {
-            console.log(e);
-            setData(e?.AHVoters ? e.AHVoters : []);
-        });
-    };
-
-    const header = (
-        <div className="flex flex-column md:flex-row md:justify-content-between md:align-items-center">
-            <h5 className="m-0">Voter Audit History</h5>
-            <span className="block mt-2 md:mt-0 p-input-icon-left">
-                <i className="pi pi-search" />
-                {/* <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Filter" /> */}
-            </span>
-        </div>
-    );
-
-    function VoterDetails() {
-        var gender = JSON?.parse(localStorage.getItem("genders"))?.filter((e) => e.id === selectedUser?.Gender)[0]?.description;
-
-        return [
-            {
-                name: "Firstname:",
-                value: selectedUser?.Firstname,
-            },
-            {
-                name: "Surname:",
-                value: selectedUser?.Surname,
-            },
-            {
-                name: "Gender:",
-                value: gender,
-            },
-            {
-                name: "Date of birth:",
-                value: selectedUser?.DayBirth,
-            },
-            {
-                name: "Address:",
-                value: selectedUser?.Address,
-            },
-            {
-                name: "Email:",
-                value: selectedUser?.Email_address,
-            },
-            {
-                name: "ContactNumber:",
-                value: selectedUser?.Contact_Number,
-            },
-        ];
+  const submitForm = () => {
+    if (isNaN(idNumber.trim())) {
+      toast.current.show({
+        severity: 'error',
+        summary: 'Error Message',
+        detail: 'please fill the required field',
+        life: 3000,
+      })
+      return false
     }
+    setLoading(true)
+    voterAuditHistory
+      .getAuditHistoryByID(idNumber)
+      .then((e) => {
+        console.log(e)
+        setData(e?.AHVoters ? e.AHVoters : [])
+        setLoading(false)
+      })
+      .catch((e) => setLoading(false))
+  }
 
-    function RegistrationDetails() {
-        return [
-            {
-                name: "ID Number:",
-                value: selectedUser?.IDNumber,
-            },
-            {
-                name: "Registration Number:",
-                value: selectedUser?.Surname,
-            },
-            {
-                name: "Date of Issue:",
-                value: selectedUser?.DateOfIssue,
-            },
-            {
-                name: "Date of Expiry:",
-                value: selectedUser?.DateOfExpiry,
-            },
-            {
-                name: "Date Registered",
-                value: selectedUser?.DateRegistered,
-            },
-            {
-                name: "Place of Issue",
-                value: selectedUser?.PlaceOfIssue,
-            },
-        ];
-    }
+  const header = (
+    <div className="flex flex-column md:flex-row md:justify-content-between md:align-items-center">
+      <h5 className="m-0">Voter Audit History</h5>
+      <span className="block mt-2 md:mt-0 p-input-icon-left">
+        <i className="pi pi-search" />
+        {/* <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Filter" /> */}
+      </span>
+    </div>
+  )
 
-    return (
-        <div className="grid">
-            <div className="col-12">
-                <Toast ref={toast} />
-                <div>
-                    <Toolbar
-                        className="mb-4"
-                        left={
-                            <div>
-                                <span className="block mt-2 md:mt-0 p-input-icon-left">
-                                    <i className="pi pi-search" />
-                                    <InputText type="search" placeholder="Search by ID Number" value={idNumber} onInput={(e) => setIdNumber(e.target.value)} />
-                                    <Button className="p-button-success ml-4" label="Search" onClick={submitForm} />
-                                </span>
-                            </div>
-                        }
-                    ></Toolbar>
-                </div>
-                <div className="card  p-align-stretch vertical-container" style={{ height: "calc(100vh - 9rem)" }}>
-                    <Dialog
-                        header="Voter Audit History"
-                        visible={showDialog}
-                        style={{ width: "50%", height: "100vh" }}
-                        modal
-                        onHide={(e) => {
-                            setShowDialog(false);
-                        }}
-                    >
-                        <TabView>
-                            <TabPanel header="Voters Details">
-                                <DataTable size="small" scrollable={true} value={VoterDetails()} dataKey="id" responsiveLayout="scroll" resizableColumns>
-                                    <Column style={{ width: "100px" }} field="name" body={(e) => <b>{e.name}</b>}></Column>
-                                    <Column field="value"></Column>
-                                </DataTable>
-                            </TabPanel>
-                            <TabPanel header="Images">No Content</TabPanel>
-                            <TabPanel header="Registration Details">
-                                <DataTable size="small" scrollable={true} value={RegistrationDetails()} dataKey="id" responsiveLayout="scroll" resizableColumns>
-                                    <Column style={{ width: "100px" }} field="name" body={(e) => <b>{e.name}</b>}></Column>
-                                    <Column field="value"></Column>
-                                </DataTable>
-                            </TabPanel>
-                            <TabPanel header="Anomalies">No Content</TabPanel>
-                            <TabPanel header="Objections">No Content</TabPanel>
-                        </TabView>
-                    </Dialog>
+  function VoterDetails() {
+    var gender = JSON?.parse(localStorage.getItem('genders'))?.filter(
+      (e) => e.id === selectedUser?.Gender,
+    )[0]?.description
 
-                    <DataTable
-                        size="small"
-                        scrollable={true}
-                        value={data}
-                        dataKey="id"
-                        paginator
-                        rows={10}
-                        rowsPerPageOptions={[5, 10, 25]}
-                        className="datatable-responsive"
-                        currentPageReportTemplate="Showing {first} to {last} of {totalRecords} voter audit history"
-                        emptyMessage="No voter audit history."
-                        header={header}
-                        responsiveLayout="scroll"
-                        resizableColumns
-                        columnResizeMode="expand"
-                        filters={filters}
-                        filterDisplay="menu"
-                        globalFilterFields={["name", "Firstname", "IDNumber"]}
-                    >
-                        <Column field="Firstname" header="Name" sortable></Column>
-                        <Column filterField="Surname" field="Surname" header="Surname" sortable></Column>
-                        <Column field="IDNumber" header="IDNumber"></Column>
-                        <Column
-                            field="action"
-                            header="Action"
-                            body={(item) => (
-                                <>
-                                    <Button
-                                        onClick={(e) => {
-                                            setShowDialog(true);
-                                            setSelectedUser(item);
-                                        }}
-                                        tooltip="Click to View"
-                                        icon={"pi pi-eye"}
-                                        tooltipOptions={{ position: "top" }}
-                                        className=" p-button-rounded mr-2"
-                                    />
-                                </>
-                            )}
-                        ></Column>
-                    </DataTable>
-                </div>
-            </div>
+    return [
+      {
+        name: 'Firstname:',
+        value: selectedUser?.Firstname,
+      },
+      {
+        name: 'Surname:',
+        value: selectedUser?.Surname,
+      },
+      {
+        name: 'Gender:',
+        value: gender,
+      },
+      {
+        name: 'Date of birth:',
+        value: selectedUser?.DayBirth,
+      },
+      {
+        name: 'Address:',
+        value: selectedUser?.Address,
+      },
+      {
+        name: 'Email:',
+        value: selectedUser?.Email_address,
+      },
+      {
+        name: 'ContactNumber:',
+        value: selectedUser?.Contact_Number,
+      },
+    ]
+  }
+
+  function RegistrationDetails() {
+    return [
+      {
+        name: 'ID Number:',
+        value: selectedUser?.IDNumber,
+      },
+      {
+        name: 'Registration Number:',
+        value: selectedUser?.Surname,
+      },
+      {
+        name: 'Date of Issue:',
+        value: selectedUser?.DateOfIssue,
+      },
+      {
+        name: 'Date of Expiry:',
+        value: selectedUser?.DateOfExpiry,
+      },
+      {
+        name: 'Date Registered',
+        value: selectedUser?.DateRegistered,
+      },
+      {
+        name: 'Place of Issue',
+        value: selectedUser?.PlaceOfIssue,
+      },
+    ]
+  }
+
+  return (
+    <div className="grid">
+      <div className="col-12">
+        <Toast ref={toast} />
+        <div>
+          <Toolbar
+            className="mb-4"
+            left={
+              <div>
+                <span className="block mt-2 md:mt-0 p-input-icon-left">
+                  <i className="pi pi-search" />
+                  <InputText
+                    type="search"
+                    placeholder="Search by ID Number"
+                    value={idNumber}
+                    onInput={(e) => setIdNumber(e.target.value)}
+                  />
+                  <Button
+                    className="p-button-success ml-4"
+                    label="Search"
+                    onClick={submitForm}
+                  />
+                </span>
+              </div>
+            }
+          ></Toolbar>
         </div>
-    );
-};
+        <div
+          className="card  p-align-stretch vertical-container"
+          style={{ height: 'calc(100vh - 9rem)' }}
+        >
+          <Dialog
+            header="Voter Audit History"
+            visible={showDialog}
+            style={{ width: '50%', height: '100vh' }}
+            modal
+            onHide={(e) => {
+              setShowDialog(false)
+            }}
+          >
+            <TabView>
+              <TabPanel header="Voters Details">
+                <DataTable
+                  size="small"
+                  scrollable={true}
+                  value={VoterDetails()}
+                  dataKey="id"
+                  responsiveLayout="scroll"
+                  resizableColumns
+                >
+                  <Column
+                    style={{ width: '100px' }}
+                    field="name"
+                    body={(e) => <b>{e.name}</b>}
+                  ></Column>
+                  <Column field="value"></Column>
+                </DataTable>
+              </TabPanel>
+              <TabPanel header="Images">No Content</TabPanel>
+              <TabPanel header="Registration Details">
+                <DataTable
+                  size="small"
+                  scrollable={true}
+                  value={RegistrationDetails()}
+                  dataKey="id"
+                  responsiveLayout="scroll"
+                  resizableColumns
+                >
+                  <Column
+                    style={{ width: '100px' }}
+                    field="name"
+                    body={(e) => <b>{e.name}</b>}
+                  ></Column>
+                  <Column field="value"></Column>
+                </DataTable>
+              </TabPanel>
+              <TabPanel header="Anomalies">No Content</TabPanel>
+              <TabPanel header="Objections">No Content</TabPanel>
+            </TabView>
+          </Dialog>
+
+          <DataTable
+          loading={loading}
+            size="small"
+            scrollable={true}
+            value={data}
+            dataKey="id"
+            paginator
+            rows={10}
+            rowsPerPageOptions={[5, 10, 25]}
+            className="datatable-responsive"
+            currentPageReportTemplate="Showing {first} to {last} of {totalRecords} voter audit history"
+            emptyMessage="No voter audit history."
+            header={header}
+            responsiveLayout="scroll"
+            resizableColumns
+            columnResizeMode="expand"
+            filters={filters}
+            filterDisplay="menu"
+            globalFilterFields={['name', 'Firstname', 'IDNumber']}
+          >
+            <Column field="Firstname" header="Name" sortable></Column>
+            <Column
+              filterField="Surname"
+              field="Surname"
+              header="Surname"
+              sortable
+            ></Column>
+            <Column field="IDNumber" header="IDNumber"></Column>
+            <Column
+              field="action"
+              header="Action"
+              body={(item) => (
+                <>
+                  <Button
+                    onClick={(e) => {
+                      setShowDialog(true)
+                      setSelectedUser(item)
+                    }}
+                    tooltip="Click to View"
+                    icon={'pi pi-eye'}
+                    tooltipOptions={{ position: 'top' }}
+                    className=" p-button-rounded mr-2"
+                  />
+                </>
+              )}
+            ></Column>
+          </DataTable>
+        </div>
+      </div>
+    </div>
+  )
+}
