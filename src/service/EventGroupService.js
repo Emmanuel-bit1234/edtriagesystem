856fc25be47f25d5c7065fb0f7b90228c4082f1c
olwethumlimi <suprise.mlimi97@gmail.com>
2022-03-03 13:49:03 +0200

    config added

--- src/service/EventGroupService.js
+++ src/service/EventGroupService.js
@@ -1,27 +1,33 @@
-import axios from "axios";
-import { NET_IP } from "../config/Config";
-
+import axios from 'axios'
+import { NET_IP } from '../config/Config'
 
 export default function EventGroupService() {
-    
-  this.deActivateEventGroup  = (id, reason) => {
+  this.deActivateEventGroup = (id, reason) => {
     var url = `${NET_IP}/EventManagement/DeleteEventGroup?id=${id}&StatusReason=${reason}`
     return axios.get(url).then((response) => response.data)
   }
-    this.getAllEventGroups = () => {
-        var url = `${NET_IP}/EventManagement/EventGroupIndex`;
-        return axios.get(url).then((response) => response.data.EventGroups);
-    };
-    this.createEventGroup = (data) => {
-        var url = `${NET_IP}/EventManagement/InsertEventGroup`;
-        return axios.post(url, data).then((response) => response.data);
-    };
-    // this.deActivateEventGroup = (id) => {
-    //     var url = `${NET_IP}/EventManagement/EventGroupIndex/${id}`;
-    //     return axios.get(url).then((response) => response.data);
-    // };
-    // this.activateEventGroup = (id) => {
-    //     var url = `${NET_IP}/EventManagement/EventGroupIndex/${id}`;
-    //     return axios.get(url).then((response) => response.data);
-    // };
+  this.getAllEventGroups = () => {
+    var url = `${NET_IP}/EventManagement/EventGroupIndex`
+    return axios.get(url).then((response) => response.data.EventGroups)
+  }
+  this.createEventGroup = (data) => {
+    let config = {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
+        'Access-Control-Allow-Headers':
+          'Content-Type, Authorization, X-Requested-With',
+      },
+    }
+    var url = `${NET_IP}/EventManagement/InsertEventGroup`
+    return axios.post(url, data,config).then((response) => response.data)
+  }
+  // this.deActivateEventGroup = (id) => {
+  //     var url = `${NET_IP}/EventManagement/EventGroupIndex/${id}`;
+  //     return axios.get(url).then((response) => response.data);
+  // };
+  // this.activateEventGroup = (id) => {
+  //     var url = `${NET_IP}/EventManagement/EventGroupIndex/${id}`;
+  //     return axios.get(url).then((response) => response.data);
+  // };
 }
