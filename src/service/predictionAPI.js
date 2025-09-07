f82e1193da07ec5ed75a3f06e779d65e9b4e5ee4
Emmanuel Mbuya <emmechcool@gmail.com>
2025-09-08 00:43:59 +0200

    thecommit1

--- src/service/predictionAPI.js
+++ src/service/predictionAPI.js
@@ -3,25 +3,11 @@ import axios from "axios";
 export default function PredictionAPI() {
     this.getPrediction = (data) => {
         var url = "https://1c890312931d.ngrok-free.app/model1";
-        return axios
-            .post(url, data, {
-                headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                },
-            })
-            .then((response) => response.data);
+        return axios.post(url, data, { withCredentials: true }).then((response) => response.data);
     };
-
     this.getAllPredictions = () => {
-        var url = "https://1c890312931d.ngrok-free.app/get-predictions";
-        return axios
-            .get(url, {
-                headers: {
-                    Accept: "application/json",
-                },
-            })
-            .then((response) => response.data);
+        var url = `https://1c890312931d.ngrok-free.app/get-predictions`;
+        return axios.get(url).then((response) => response.data);
     };
 
     this.getAllPredictions1 = async () => {
